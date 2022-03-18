87bd11f71477bbc876ffe5e9df6be3ba84b6d7bc
Jason Watkins <goushoryuken@gmail.com>
2022-03-18 13:39:10 -0700

    done

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,39 @@
+
+
+def encode(string, value)
+  new_string = ""
+  string.each_byte do |c|
+
+    if c.between?(65, 90)
+          c += (value % 26)
+
+          if c < 65
+            c += 26
+          elsif c > 90
+            c -= 26
+          end
+        
+        elsif c.between?(97, 122)
+          c += (value % 26)
+        
+          if c < 97
+            c += 26
+          elsif c > 122
+            c -= 26
+          end
+        end
+    new_string += c.chr       
+  end
+  new_string
+end
+
+# print 'enter string: '
+# string = gets.chomp
+# print 'enter value: '
+# value = gets.chomp.to_i
+
+# p encode(string, value)
+
+  
+
+    
